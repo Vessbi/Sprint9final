d54d93f8ac1dbf99d6a082c14e7d2cfebc6de8c6
Vitalii Kondratev <vessbi@gmail.com>
2025-06-24 19:29:50 +0300

    Sprint9:first commit

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"slices"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,28 +16,82 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	if size <= 0 {
+		return nil
+	}
+	sliceElem := make([]int, 0, size)
+
+	for i := 0; i <= size; i++ {
+		sliceElem = append(sliceElem, rand.Int())
+	}
+
+	return sliceElem
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+
+	if len(data) <= 0 {
+		return 0
+	}
+	maxNumber := slices.Max(data)
+	return maxNumber
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
+	var wg sync.WaitGroup
+	result := make(chan int, CHUNKS)
+
+	for i := 0; i < CHUNKS; i++ {
+
+		idx1 := len(data) / CHUNKS * i
+		idx2 := len(data) / CHUNKS * (i + 1)
+
+		wg.Add(1)
+		go func(idx1, idx2 int) {
+			defer wg.Done()
+			sl1 := data[idx1:idx2]
+			result <- maximum(sl1)
+
+		}(idx1, idx2)
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	maxRut := 0
+	for max_value := range result {
+		if max_value > maxRut {
+			maxRut = max_value
+		}
+	}
+	return maxRut
 	// ваш код здесь
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	RE := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
-
+	start := time.Now()
+	max := maximum(RE)
+	end := time.Now()
+	elapsed := time.Duration.Milliseconds(end.Sub(start))
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(RE)
+	end = time.Now()
+	elapsed = time.Duration.Milliseconds(end.Sub(start))
 	// ваш код здесь
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
--- main_test.go
+++ main_test.go
@@ -1,3 +1,27 @@
 package main
 
+import "testing"
+
 // Пишите тесты в этом файле
+
+func TestMaximum(t *testing.T) {
+	randSlice := [][]int{
+		{5, 10, 67, 350, -1},
+		{10},
+		{},
+	}
+	// ожидаемые значения для каждой последовательности
+	max := []int{350, 10, 0}
+
+	for i, list := range randSlice {
+		if maximum(list) != max[i] {
+			t.Error(i, ":", maximum(list), "!=", max[i])
+		}
+	}
+}
+
+func TestGenerateRandomElements(t *testing.T) {
+	if generateRandomElements(0) != nil {
+		t.Error("slide size cannot be less than 0")
+	}
+}
